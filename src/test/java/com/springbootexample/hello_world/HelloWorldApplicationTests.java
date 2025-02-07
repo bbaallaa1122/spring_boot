7d4aa7aeb4f13eaf50334e7fad77b74a98fb80f8
Bala Ganesh <balaganeshk3008@gmail.com>
2025-02-07 22:47:49 +0530

    email feature added

--- src/test/java/com/springbootexample/hello_world/HelloWorldApplicationTests.java
+++ src/test/java/com/springbootexample/hello_world/HelloWorldApplicationTests.java
@@ -3,11 +3,13 @@ package com.springbootexample.hello_world;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class HelloWorldApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        fail("Forcing test failure in GitHub Actions");
+    }
 }
